9e1649f1dafb179c19b6de92d662a91a2c33906e
PauloSuriani <paulosuriani@gmail.com>
2021-08-26 19:31:38 -0300

    [course] exercicio 04

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-3.js
@@ -4,14 +4,17 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let numbersSum = 0;
 
-for (let i = 0; i < numbers.length; i += 1)
+for (let i = 0; i < numbers.length; i += 1){
     numbersSum += numbers[i];
+}
+
 
 let numbersLength = numbers.length + 1;
 
 let avgResult = 0;
 
 avgResult = numbersSum / numbersLength; // calculo da média: Soma total / quantidade
+//A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
 
 console.log(avgResult);
 
