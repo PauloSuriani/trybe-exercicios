d0d228105f8a253bbca1548f993990d15747e3f2
PauloSuriani <paulosuriani@gmail.com>
2021-10-04 18:50:46 -0300

    Pratica Exercicio-02

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/pratica.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/pratica.js
@@ -1,8 +1,8 @@
-const newEmployees = () => {
+const newEmployees = (callback) => {
   const employees = {
-    id1: employeeData('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: employeeData('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: employeeData('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
 };
@@ -24,4 +24,40 @@ const employeeData = (name) => {
   }
 };
 
-console.log(newEmployees());
+// console.log(newEmployees(employeeData));
+
+
+// 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. 
+// sta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros
+//  o número apostado e uma função que checa se o número apostado 
+//  é igual ao número sorteado. O retorno da sua HOF deve ser uma string 
+//  (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+const sorteio = (callback, nro) => {
+  const nroSorteado = Math.ceil(Math.random()*5);
+  const resultado = callback(nro, nroSorteado);
+  return resultado;
+};
+
+const foiSorteado = (nroApostado, nroSorteado) => {
+  if (nroApostado === nroSorteado) {
+    return 'Parabéns você ganhou';
+  } else return 'Tente novamente';
+};
+
+// console.log(sorteio(foiSorteado, 3));
+
+// 3 - Crie uma HOF que receberá três parâmetros. 
+// O primeiro será um array de respostas corretas (Gabarito), 
+// o segundo será um array de respostas a serem verificadas 
+// (respostas da pessoa estudante) e o terceiro é uma função que checa 
+// se as respostas estão corretas e faz a contagem da 
+// pontuação final recebida pela pessoa estudante. 
+// Ao final a HOF deve retornar o total da contagem de respostas certas.
+
+// Quando a resposta for correta a contagem sobe 1 ponto,
+//  quando for incorreta desce 0.5 pontos, e quando não houver 
+//  resposta ("N.A") não altera-se a contagem.
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
