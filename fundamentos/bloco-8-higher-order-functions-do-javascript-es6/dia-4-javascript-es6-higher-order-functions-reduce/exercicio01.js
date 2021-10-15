1aa6a18926ffea2bb5fc020fcb672b5491802654
PauloSuriani <paulosuriani@gmail.com>
2021-10-14 21:21:06 -0300

    Exercicio-05

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicio01.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicio01.js
@@ -139,3 +139,23 @@ function longestNamedBook() {
 }
 
 console.table(longestNamedBook());
+
+// 5 - Dada o array de nomes, retorne a quantidade de vezes em que 
+// aparecem a letra "a" maiúscula ou minúscula.
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA() {
+  let novoNome;
+  return names.reduce((accumulator, nome) => {
+    novoNome = nome.toLowerCase().split('');
+    novoNome.forEach((letra) => letra === 'a' ? accumulator += 1 : '');
+    return accumulator;
+  }, 0);
+}
+
+console.log(containsA());
