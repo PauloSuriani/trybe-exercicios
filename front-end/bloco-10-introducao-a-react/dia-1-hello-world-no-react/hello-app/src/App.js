710f99814c0b5b8dae40d08552e7ff3e84bc029b
PauloSuriani <paulosuriani@gmail.com>
2021-10-27 22:03:42 -0300

    feat: Adiciona tarefa a pagina

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/hello-app/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/hello-app/src/App.js
@@ -9,22 +9,23 @@ const Task = (value) => {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ol>{Task('React first project')}</ol>
+    // <div className="App">
+    //   {/* <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header> */}
+    // </div>
   );
 }
 
