1f7117e1027a15e0e6a57b67cd0ca12a623f798c
PauloSuriani <paulosuriani@gmail.com>
2021-10-27 22:52:20 -0300

    feat: Adiciona Header e Content

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-03/src/Content.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class Content extends React.Component {
+  render() {
+    const conteudos = [
+      {
+        conteudo: 'High Order Functions',
+        bloco: 8,
+        status: 'Aprendido'
+      },
+      {
+        conteudo: 'Composicao de Componentes',
+        bloco: 11,
+        status: 'Aprendendo',
+      },
+      {
+        conteudo: 'Composicao de Estados',
+        bloco: 12,
+        status: 'Aprenderei'
+      },
+      {
+        conteudo: 'Redux',
+        bloco: 16,
+        status: 'Aprenderei'
+      },
+    ];
+
+    return (
+      <li>{conteudos.map((conteudo) => 
+        `O conteúdo é: ${conteudo.conteudo}
+        Status: ${conteudo.status}
+        Bloco: ${conteudo.bloco}`)}
+      </li>
+    )
+  }
+}
+
+export default Content;
