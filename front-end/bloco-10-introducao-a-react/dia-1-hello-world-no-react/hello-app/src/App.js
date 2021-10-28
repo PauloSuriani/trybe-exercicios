05ceb18ddc4a58aa8bac2ce97c209479b7680191
PauloSuriani <paulosuriani@gmail.com>
2021-10-27 22:09:31 -0300

    feat: Cria lista a partir do array 'taskList'

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/hello-app/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/hello-app/src/App.js
@@ -1,31 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
-const Task = (value) => {
+const Task = (taskList) => {
   return (
-    <li>{value}</li>
+    taskList.map((value) => <li>{value}</li>)
   );
 }
 
 function App() {
+  const taskList = ['React', 'NPM', 'JSX', 'Classes'];
+
   return (
-    <ol>{Task('React first project')}</ol>
-    // <div className="App">
-    //   {/* <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header> */}
-    // </div>
+    <ol>{Task(taskList)}</ol>
   );
 }
 
