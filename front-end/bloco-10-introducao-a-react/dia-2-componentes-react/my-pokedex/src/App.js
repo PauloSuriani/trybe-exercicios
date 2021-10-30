d955b369ca6a518920bec5f044c7f25ef3ff14e1
PauloSuriani <paulosuriani@gmail.com>
2021-10-30 00:12:18 -0300

    feat: Estlizaçao da pokedex em  App.css

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
@@ -7,14 +7,16 @@ import pokemons from './data';
 function App() {
   return (
     <div className="App">
-      {pokemons.map((pokemon) => (
-        <Pokemon name={ pokemon.name }
-        type={ pokemon.type }
-        value={ pokemon.averageWeight.value }
-        measurementUnit={ pokemon.averageWeight.measurementUnit }
-        image={ pokemon.image }/>
-      ))}   
-
+      <h1>Pokedex</h1>
+      <div className="poke-dex">
+        {pokemons.map((pokemon) => (
+          <Pokemon name={ pokemon.name }
+          type={ pokemon.type }
+          value={ pokemon.averageWeight.value }
+          measurementUnit={ pokemon.averageWeight.measurementUnit }
+          image={ pokemon.image }/>
+        ))}   
+      </div>
     </div>
   );
 }
