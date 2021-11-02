bfaa01f62bc3f6589a780c2b2a88f5cbb4a1eb92
PauloSuriani <paulosuriani@gmail.com>
2021-11-01 22:28:14 -0300

    feat: faz fetch na API coincap e mostra informaçoes

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/api.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/api.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
 const coinCapFetch = async () => {
   const url = `https://api.coincap.io/v2/assets`;
@@ -10,6 +10,24 @@ const coinCapFetch = async () => {
 
 const getCoinsArray = async () => {
   const fetchResult = await coinCapFetch();
+  const coins = fetchResult;
+
+  const coinsUl = document.querySelector('.cripto-list');
+
+  coins.forEach((coin, index) => {
+    if (index < 10){
+      console.log(coin);
+    const newElement = document.createElement('li');
+    const usdPrice = Number(coin.priceUsd);
+
+    newElement.innerText = `${coin.name} (${coin.symbol}): ${usdPrice.toFixed(2)} USD`;
+
+    coinsUl.appendChild(newElement);}
+  });
 }
 
-getCoinsArray();
+const setCoinsList = () => {
+
+}
+window.onload = () => getCoinsArray();
+// console.log(getCoinsArray());
